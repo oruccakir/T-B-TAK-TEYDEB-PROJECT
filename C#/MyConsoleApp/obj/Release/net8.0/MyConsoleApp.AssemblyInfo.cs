3b55e0f473e0fd4133fe3e0b26cef24523ce304d
oruccakir <oruccakir2525@gmail.com>
2024-01-31 18:04:39 +0300

    modbus connectionalearned and easymodbus library installed

--- C#/MyConsoleApp/obj/Release/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ C#/MyConsoleApp/obj/Release/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("try")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+346070db007761b2b2b358a0001e1b6719b1795e")]
-[assembly: System.Reflection.AssemblyProductAttribute("try")]
-[assembly: System.Reflection.AssemblyTitleAttribute("try")]
+[assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
+[assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
--- Learning/try/try.cs
+++ Learning/try/try.cs
@@ -1,10 +1,19 @@
 
+
+using EasyModbus;
 using System;
 class Modbus{
 
-
+ 
     static void Main(string [] args){
+
+        string ipAddress = "klvnlkd";
+        int portNumber = 0;
+
+        //ModbusClient client = new ModbusClient(ipAddress,portNumber);
+
         Console.WriteLine("Hello World");
+        Console.WriteLine("Modbus connection");
     }
 
 }
