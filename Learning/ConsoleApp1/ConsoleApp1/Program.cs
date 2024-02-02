ad7ab7da51237bfe76b158edc3b02ea4e9b32453
oruccakir <oruccakir2525@gmail.com>
2024-02-02 10:42:03 +0300

    testint

---
+++ Learning/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using EasyModbus;
+using System.IO;
+public class Test
+{
+
+    static void Main(string[] args)
+    {
+        string str = "lknsd";
+        Console.WriteLine("lkdl");
+        ModbusClient client = new ModbusClient(str);
+    }
+
+}
